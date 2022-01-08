cd31900271d41b852c6909b346d52fac92be7970
shogo4131 <shougo80697118@gmail.com>
2022-01-08 14:14:06 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  dev: {
+    baseUrl: 'http://localhost:3000',
+  },
+};
---
+++ src/components/accordion/index.tsx
@@ -0,0 +1,47 @@
+import type { VFC, ReactNode } from 'react';
+import { useState, useCallback } from 'react';
+import clsx from 'clsx';
+import ArrowDownIcon from '../../../public/arrow-down-gray-390.svg';
+import styles from './index.module.css';
+
+export type Props = {
+  className?: string;
+  headerClassName?: string;
+  contentClassName?: string;
+  header: ReactNode;
+  children: ReactNode;
+};
+
+export const Accordion: VFC<Props> = ({
+  className,
+  headerClassName,
+  contentClassName,
+  header,
+  children,
+}) => {
+  const [open, setOpen] = useState(false);
+
+  const onClickHandler = useCallback(() => setOpen((prev) => !prev), []);
+
+  return (
+    <div className={clsx(styles.root, className)}>
+      <div className={clsx(styles.header, headerClassName)}>
+        {header}
+        {/*TODO: Imageタグを使用できるようにする */}
+        <img
+          alt="arrow-down"
+          src={ArrowDownIcon}
+          className={clsx(styles.icon, { [styles.activeIcon]: open })}
+          onClick={onClickHandler}
+        />
+      </div>
+      <div
+        className={clsx(styles.content, contentClassName, {
+          [styles.active]: open,
+        })}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
