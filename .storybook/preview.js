f8c5df3f062d97472de8bfd41f02cd7e0e662346
shogo4131 <shougo80697118@gmail.com>
2022-01-08 19:42:28 +0900

    fix: add storybook setting

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import * as nextImage from 'next/image';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +9,8 @@ export const parameters = {
     },
   },
 };
+
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => <img {...props} />,
+});
