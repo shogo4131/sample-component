92efa2227804a6af989b2224fbbe56d4eb4d8005
shogo4131 <shougo80697118@gmail.com>
2022-01-10 16:40:06 +0900

    feat/add loader component

---
+++ src/components/loader/index.tsx
@@ -0,0 +1,16 @@
+import type { VFC } from 'react';
+import clsx from 'clsx';
+import styles from './index.module.css';
+
+export type Props = {
+  className?: string;
+  loaderClassName?: string;
+};
+
+export const Loader: VFC<Props> = ({ className, loaderClassName }) => {
+  return (
+    <div className={className}>
+      <div className={clsx(styles.loader, loaderClassName)}></div>
+    </div>
+  );
+};
