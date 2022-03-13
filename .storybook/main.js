af3a3d5ad83e0cd90dd2188de0714407ddf4b957
shogo4131 <shougo80697118@gmail.com>
2022-03-14 01:26:57 +0900

    feat: add absolute path setting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -8,4 +11,13 @@ module.exports = {
     'storybook-addon-next-router',
   ],
   framework: '@storybook/react',
+  webpackFinal: async (baseConfig) => {
+    baseConfig.resolve.modules = [
+      ...(baseConfig.resolve.modules || []),
+      path.resolve(__dirname, '../'),
+    ];
+
+    baseConfig.resolve.plugins = [...(baseConfig.resolve.plugins || []), new TsconfigPathsPlugin()];
+    return baseConfig;
+  },
 };
