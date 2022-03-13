3ec4010ed402a0e674660d511fb3534f010e743c
shogo4131 <shougo80697118@gmail.com>
2022-03-13 14:29:57 +0900

    fix: jest setting

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ const nextJest = require('next/jest');
 const createJestConfig = nextJest({ dir: './' });
 
 const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/components/$1',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
--- src/components/button/index.test.tsx
+++ src/components/button/index.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import { Button } from './index';
 
 describe('button component test', () => {
