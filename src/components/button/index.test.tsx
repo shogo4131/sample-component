4f1fee367b160b3a00a299f88c49597bd14cba7e
shogo4131 <60390181+shogo4131@users.noreply.github.com>
2022-03-14 08:48:04 +0000

    Deploying to gh-pages from @ shogo4131/sample-component@e6d2215540b75debe557a2d10ddf8e4198ec2af2 🚀

--- src/components/button/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Button } from './index';
-
-describe('button component test', () => {
-  it('should children render test', () => {
-    render(<Button>test button</Button>);
-    expect(screen.getByText('test button')).toBeInTheDocument();
-  });
-});
--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css';
-import 'reset-css';
-import type { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
